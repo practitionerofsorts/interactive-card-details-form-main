52e9ac55a0bc6c308712375d1ea34d7465bef920
Promise Okeke <plastamasta21@gmail.com>
2022-08-15 01:41:48 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const confirmButton = document.querySelector(".confirm-button");
+const cardUserName = document.getElementById("carduser-name");
+const cardNumber = document.querySelector(".card-number");
+const incorrect = document.querySelectorAll(".incorrect");
+const monthInput = document.querySelector(".month-input");
+const yearInput = document.querySelector(".year-input");
+const cvcInput = document.querySelector(".cvc-input");
+const inputError = document.querySelectorAll("input");
+const doubleNameRegex =
+  /^([a-zA-Z]{1,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{1,}\s?([a-zA-Z]{1,})?)/;
+const monthInputRegex = /^(0?[1-9]|1[012])$/;
+const yearInputRegex = /(?:(?:19|20)[0-9]{2})/;
+
+cardUserName.addEventListener("input", () => {
+  if (!cardUserName.value.match(doubleNameRegex)) {
+    incorrect[0].classList.add("active");
+    inputError[0].classList.add("active");
+    incorrect[0].innerHTML = "Please put in a Firstname and a Lastname 😁";
+  } else {
+    incorrect[0].classList.remove("active");
+    inputError[0].classList.remove("active");
+  }
+});
+
+// makes sure that the user only inputs numbers and not alphabets
+cardNumber.addEventListener("input", (e) => {
+  e.target.value = e.target.value
+    .replace(/[^\dA-Z]/g, "")
+    .replace(/(.{4})/g, "$1 ")
+    .trim();
+});
+monthInput.addEventListener("input", (e) => {
+  e.target.value = e.target.value
+    .replace(/[^\dA-Z]/g, "")
+    .replace(/(.{4})/g, "$1 ")
+    .trim();
+});
+yearInput.addEventListener("input", (e) => {
+  e.target.value = e.target.value
+    .replace(/[^\dA-Z]/g, "")
+    .replace(/(.{4})/g, "$1 ")
+    .trim();
+});
+cvcInput.addEventListener("input", (e) => {
+  e.target.value = e.target.value
+    .replace(/[^\dA-Z]/g, "")
+    .replace(/(.{4})/g, "$1 ")
+    .trim();
+});
+
+confirmButton.addEventListener("click", () => {
+  if (cardUserName.value == "") {
+    incorrect[0].classList.add("active");
+    incorrect[0].innerHTML = "Please put in a Firstname and a Lastname 😁";
+  }
+  if (cardNumber.value.length <= 16) {
+    incorrect[1].classList.add("active");
+    inputError[1].classList.add("active");
+    incorrect[1].innerHTML = "Your card number should be 16";
+  } else {
+    incorrect[1].classList.remove("active");
+    inputError[1].classList.remove("active");
+  }
+  if (cardNumber.value == "") {
+    inputError[1].classList.add("active");
+    incorrect[1].innerHTML = "Please put in a YOUR card number";
+  } else {
+  }
+  if (!monthInput.value.match(monthInputRegex)) {
+    incorrect[2].classList.add("active");
+    incorrect[2].innerHTML = "Please put in a valid date";
+  } else {
+    incorrect[2].classList.remove("active");
+  }
+  if (monthInput.value == "" || yearInput.value == "") {
+    incorrect[2].innerHTML = "Can't be empty";
+  }
+  if (cvcInput.value == "") {
+    incorrect[3].classList.add("active");
+    incorrect[3].innerHTML = "Can't be empty";
+  } else {
+    incorrect[3].classList.remove("active");
+  }
+});
